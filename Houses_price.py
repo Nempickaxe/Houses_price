af8ef384fe025a192a16f88828b3af9ec279cdb2
Nemish1 <nemishkanwar@gmail.com>
2017-01-14 18:08:03 +0530

    no_null_objects and sales_vs_col function
    no_null_objects     Returns rows with no NaNs
    sales_vs_col Plot column v/s SalePrice on y-axis

--- Houses_price.py
+++ Houses_price.py
@@ -6,6 +6,26 @@ Created on Sat Jan 14 17:09:09 2017
 """
 #House prices sample
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 #%%
 House_price_train = pd.read_csv('train.csv')
 House_price_test = pd.read_csv('test.csv')
+
+def no_null_objects(data, columns=None):
+    """
+    Returns rows with no NaNs
+    """
+    if columns is None:
+        columns = data.columns
+    return data[np.logical_not(np.any(data[columns].isnull().values, axis=1))]
+
+def sales_vs_col(col , HoP = House_price_train):
+    '''
+    Plot column v/s SalePrice on y-axis
+    '''
+    c = HoP[col]
+    d = HoP[['SalePrice']]
+    plt.plot(c, d) # forgot how to plot in python
+sales_vs_col(col = ['LandContour'])
+
