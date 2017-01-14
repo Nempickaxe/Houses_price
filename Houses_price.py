0f2825ad21438cb189efe7a1118d261f74fca978
Nemish1 <nemishkanwar@gmail.com>
2017-01-14 17:22:05 +0530

    test2_adding, ignoring and updates

--- Houses_price.py
+++ Houses_price.py
@@ -5,3 +5,7 @@ Created on Sat Jan 14 17:09:09 2017
 @author: Nemish
 """
 #House prices sample
+import pandas as pd
+#%%
+House_price_train = pd.read_csv('train.csv')
+House_price_test = pd.read_csv('test.csv')
