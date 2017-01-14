93b716dcc1adfd56044d1f8613625a7bc2a368c1
Nemish1 <nemishkanwar@gmail.com>
2017-01-14 19:14:51 +0530

    convert_to_string fnc

--- Houses_price.py
+++ Houses_price.py
@@ -12,6 +12,12 @@ from matplotlib import pyplot as plt
 House_price_train = pd.read_csv('train.csv')
 House_price_test = pd.read_csv('test.csv')
 
+def convert_to_string(df, columns=None):
+    '''
+    converts a column to string
+    '''
+    df[columns] =  map(lambda x: str(x), df[columns])
+
 def no_null_objects(data, columns=None):
     """
     Returns rows with no NaNs
@@ -28,4 +34,7 @@ def sales_vs_col(col , HoP = House_price_train):
     d = HoP[['SalePrice']]
     plt.plot(c, d) # forgot how to plot in python
 sales_vs_col(col = ['LandContour'])
-
+#%%
+convert_to_string(House_price_train, 'Id')
+convert_to_string(House_price_test, 'Id')
+summary = House_price_train.describe()
