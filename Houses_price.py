d9880b3dbf683a4247070ca4fe85e0c816702326
Nemish1 <nemishkanwar@gmail.com>
2017-01-15 22:07:38 +0530

    check_null, barh_val, convert_to_catg, sales_vs_col
    check_null(Dataframe): Returns columns with nulls
    barh_val(a): horizontal bar graph with values at the end
    convert_to_catg(col, HoP = House_price_train): Converts a column to
    category data type
    sales_vs_col(col , HoP = House_price_train): Plot column v/s SalePrice
    on y-axis

--- Houses_price.py
+++ Houses_price.py
@@ -8,13 +8,16 @@ Created on Sat Jan 14 17:09:09 2017
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid", color_codes=True)
 #%%
 House_price_train = pd.read_csv('train.csv')
 House_price_test = pd.read_csv('test.csv')
-
+HoP = House_price_train[:] # external copy, just for reference
+#%%
 def convert_to_string(df, columns=None):
     '''
-    converts a column to string
+    Converts a column to string
     '''
     df[columns] =  map(lambda x: str(x), df[columns])
 
@@ -30,11 +33,44 @@ def sales_vs_col(col , HoP = House_price_train):
     '''
     Plot column v/s SalePrice on y-axis
     '''
-    c = HoP[col]
-    d = HoP[['SalePrice']]
-    plt.plot(c, d) # forgot how to plot in python
-sales_vs_col(col = ['LandContour'])
+    sns.boxplot(x = col, y = 'SalePrice', data = HoP)
+sales_vs_col(col = 'LandContour')
+#%%
+def convert_to_catg(col, HoP = House_price_train):
+   '''
+   Converts a column to category data type
+   '''
+   HoP[col] = HoP[col].astype('category')
+
 #%%
 convert_to_string(House_price_train, 'Id')
 convert_to_string(House_price_test, 'Id')
 summary = House_price_train.describe()
+
+# categorical variables
+cat_var = House_price_train.dtypes[House_price_train.dtypes == object].index.tolist()
+
+#Data separated into categorical and numerical df
+Data_catg = House_price_train[cat_var]
+Data_num = House_price_train[list(set(HoP.columns)-set(cat_var))]
+
+#%%
+#Checking if null values present
+def check_null(Dataframe):
+    '''
+    Returns columns with nulls
+    '''
+    return Dataframe.isnull().sum()[Dataframe.isnull().sum()>0]
+def barh_val(a):
+    '''
+    horizontal bar graph with values at the end
+    '''
+    ax = a.plot.barh(color = '#FFA700', edgecolor = '#0F0A00') 
+    for p in ax.patches:
+        width = p.get_width()
+        ax.text(width+ 3 , p.get_y() + p.get_height()/2,  '%d'%width, color='#0F0A00',
+                fontweight = 'bold', fontsize = 20, \
+                verticalalignment ='center')
+barh_val(check_null(Data_num))
+barh_val(check_null(Data_catg))
+##convert_to_catg(col = 'LandContour')
