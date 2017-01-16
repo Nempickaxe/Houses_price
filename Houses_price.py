263733662082b07dd1af6d9a1d049f02d56eec72
Nemish1 <Nemish1@users.noreply.github.com>
2017-01-16 18:07:41 +0530

    tweaks in graph function; removed null colmns
    now graph function is super awesome, removed columns with too many null values

--- Houses_price.py
+++ Houses_price.py
@@ -61,17 +61,31 @@ def check_null(Dataframe):
     Returns columns with number of nulls
     '''
     return Dataframe.isnull().sum()[Dataframe.isnull().sum()>0]
-def barh_val(a):
+def barh_val(Data):
     '''
     horizontal bar graph with values at the end
     '''
-    ax = a.plot.barh(color = '#FFA700', edgecolor = '#0F0A00') 
+    
+    a= check_null(Data)
+    max_width = int((np.logical_not(Data.isnull()).sum()+ Data.isnull().sum()).mean()) 
+    #is there any easier way to get count
+    
+    ax = a.plot.barh(color = '#FFA700', edgecolor = '#0F0A00', xlim = (0,max_width))
+    ax.grid(False)
+    ax.set_title('Barplot of columns with null values: number of null values')
     for p in ax.patches:
         width = p.get_width()
-        ax.text(width+ 3 , p.get_y() + p.get_height()/2,  '%d'%width, color='#0F0A00',
+        percent = width/max_width*100
+        ax.text(width+ 3 , p.get_y() + p.get_height()/2,  '{0}'.format(int(width)), color='#0F0A00',
                 fontweight = 'bold', fontsize = 20, \
                 verticalalignment ='center')
-barh_val(check_null(Data_num))
-barh_val(check_null(Data_catg))
+        ax.text(0 , p.get_y() + p.get_height()/2,  '{0}%'.format(int(percent)), color='#000000',
+                fontweight = 'bold', fontsize = 9, \
+                verticalalignment ='center')
+barh_val(Data_num)
+barh_val(Data_catg)
 #%%
+#Removing columns with too many null values 
+rem = check_null(Data_catg)[check_null(Data_catg)/1460>.5].index
+Data_catg_significant = Data_catg.drop(rem, axis = 1)
 ##convert_to_catg(col = 'LandContour')
